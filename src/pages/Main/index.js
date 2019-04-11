a793dc56c54c91138f216ded265ec0c339f2e2d4
matheus <matheus@ti>
2019-04-11 10:45:13 -0300

    upload

---
+++ src/pages/Main/index.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './styles.css';
+import logo from '../../assets/logo.svg';
+// import { Container } from './styles';
+import api from '../../services/api';
+
+
+export default class Main extends Component {
+    state = {
+        newBox:''
+    };
+
+    handleSubmit = async (e) =>{
+        e.preventDefault();
+
+        const response = await api.post('boxes',{
+            title:this.state.newBox 
+        });
+ 
+        this.props.history.push(`/box/${response.data._id}`)
+    }
+
+    handleInputChange = (e) =>{
+        this.setState({
+            newBox:e.target.value
+        });
+    }
+
+    render() {
+    return (
+      <div id="main-container">
+            <form onSubmit={this.handleSubmit} action="">
+                <img src={logo} alt="" />    
+                <input placeholder="Criar um box" value={this.state.newBox}  onChange={this.handleInputChange} />
+                <button type="submit">Criar</button>            
+            </form>
+      </div>
+    );
+  }
+}
